0b572076db12cfaa6682e63ada44e08976101342
alfaris105 <raafialfaris7@gmail.com>
2025-10-20 22:27:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  tokens: "8492430132:AAFzqOiIYvb4QqnEjyV5GokgsZxpze9mh34",  // Ubah Jadi Token Bot Mu !!!
-  owner: "1320437076", // Ubah Jadi Id Mu !!!
+  tokens: "8112257591:AAEr0bo1TWkEW6yma79QqdVrNHOXZsJvHxw",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7870923244", // Ubah Jadi Id Mu !!!
   port: "19000", // Ubah Jadi Port Panel Mu !!!
   ipvps: "157.245.196.181" // Ubah Jadi Ip Vps Mu !!!
 };
